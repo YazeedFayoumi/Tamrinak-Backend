57153a388c753930b73a6f307641cb6ef7dafe6d
Yazeed Fayoumi <140242342+YazeedFayoumi@users.noreply.github.com>
2025-03-21 13:33:58 +0300

    db-start

---
+++ Tamrinak-API/DataAccess/DatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Tamrinak_API.DataAccess
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) 
+        {
+        }
+
+
+    }
+}
