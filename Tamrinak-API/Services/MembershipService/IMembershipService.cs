ce31fc6984ad1dad7ebeec500ed77d80943474e2
Yazeed Fayoumi <140242342+YazeedFayoumi@users.noreply.github.com>
2025-05-06 02:22:35 +0300

    membership

--- Tamrinak-API/Services/MembershipService/IMembershipService.cs
+++ Tamrinak-API/Services/MembershipService/IMembershipService.cs
@@ -1,6 +1,13 @@
-﻿namespace Tamrinak_API.Services.MembershipService
+﻿using Tamrinak_API.DTO.MembershipDtos;
+
+namespace Tamrinak_API.Services.MembershipService
 {
-    public class IMembershipService
+    public interface IMembershipService
     {
+        Task<MembershipDto> AddMembershipAsync(AddMembershipDto dto, string userEmail);
+        Task<List<MembershipDto>> GetUserMembershipsAsync(string userEmail);
+        Task<MembershipDto> GetMembershipByIdAsync(int id);
+        Task CancelMembershipAsync(int id, string userEmail);
+        Task DeleteMembershipAsync(int id, string userEmail);
     }
 }
