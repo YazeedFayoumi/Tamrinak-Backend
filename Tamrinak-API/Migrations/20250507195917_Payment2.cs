e5b5bfb4a5b083a79032eb240165afeae6fd9b4b
Yazeed Fayoumi <140242342+YazeedFayoumi@users.noreply.github.com>
2025-05-08 00:08:15 +0300

    minor

---
+++ Tamrinak-API/Migrations/20250507195917_Payment2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tamrinak_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Payment2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CancelledAt",
+                table: "Payments",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRefunded",
+                table: "Payments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefundedAt",
+                table: "Payments",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Payments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CancelledAt",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "IsRefunded",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "RefundedAt",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Payments");
+        }
+    }
+}
