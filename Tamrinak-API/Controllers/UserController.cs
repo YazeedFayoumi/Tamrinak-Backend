918b34d7afa38a74f85c50fda757e7a637c2df23
Mousa Khaleel <mousakhaleel22@gmail.com>
2025-05-20 23:43:44 +0300

    mc

--- Tamrinak-API/Controllers/UserController.cs
+++ Tamrinak-API/Controllers/UserController.cs
@@ -49,12 +49,12 @@ namespace Tamrinak_API.Controllers
 			return Ok(roles);
 		}
 
-		[HttpGet("UserRolesByEmail/{email}"), AuthorizeRole(Roles = "Admin, SuperAdmin")]
-		public async Task<ActionResult> GetUserRoles([FromBody] string email)
-		{
-			var roles = await _userService.GetUserRolesAsync(email);
-			return Ok(roles);
-		}
+		//[HttpGet("UserRolesByEmail/{email}"), AuthorizeRole(Roles = "Admin, SuperAdmin")]
+		//public async Task<ActionResult> GetUserRoles([FromBody] string email)
+		//{
+		//	var roles = await _userService.GetUserRolesAsync(email);
+		//	return Ok(roles);
+		//}
 		/*[HttpGet("BasicInfoUserList")]
 		public async Task<IActionResult> GetUsersBasicInfo()
 		{
